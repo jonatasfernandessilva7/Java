27341a52d2db8c26c26f4f6b313537286c8b7994
jonatasfernandessilva7 <jonatasfernandes@alu.ufc.br>
2023-07-13 16:02:05 -0300

    usuario pode adicionar um tamanho e numeros ao vetor

--- java-funcoes/src/Main.java
+++ java-funcoes/src/Main.java
@@ -1,20 +1,36 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args){
+
+        Scanner input = new Scanner(System.in);
 
         /*Somatório: Sua função deverá receber um vetor como parâmetro e retornar
         a soma de todos elementos do vetor, sendo que as primeiras duas e as
         últimas posições do vetor de entrada não devem ser consideradas;*/
 
-        int[] vetor = {1,2,3,4,5,6,7,8,9,10};
+        int tamanhoDefinidoPeloUsuario;
+        System.out.println("defina o tamanho do vetor: ");
+        tamanhoDefinidoPeloUsuario = input.nextInt();
+
+        int[] vetor = new int[tamanhoDefinidoPeloUsuario];
+
+        for (int i  = 0; i < tamanhoDefinidoPeloUsuario; i++){
+            System.out.println("adicione um numero ao vetor: ");
+            vetor[i] = input.nextInt();
+        }
 
         System.out.println("a soma dos numeros do vetor eh: "+somaVetor(vetor));
+
+        /*Média: Sua função deverá receber dois vetores como entrada e retornar a
+        média dos valores presentes nos vetores.*/
+
         System.out.println("a media dos numero do vetor eh: "+mediaDoVetor(vetor));
     }
 
-    public static double somaVetor(int[] vetor){
+    public static int somaVetor(int[] vetor){
 
-        double somaDosNumerosDoVetor = 0;
-
+        int somaDosNumerosDoVetor = 0;
         for (int i:vetor){
             somaDosNumerosDoVetor += i;
         }
@@ -24,9 +40,7 @@ public class Main {
     public static double mediaDoVetor(int[] vetor){
 
         double mediaDosNumemerosDoVetor;
-
         mediaDosNumemerosDoVetor = somaVetor(vetor)/vetor.length;
-
         return mediaDosNumemerosDoVetor;
     }
 }
